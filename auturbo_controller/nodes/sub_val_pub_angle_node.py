55cef871ff7c27b913198df2f02bfde19ea35eb9
SionJeon <57317636+jeonsion@users.noreply.github.com>
2023-07-25 15:01:18 +0900

    Update sub_val_pub_angle_node.py

--- auturbo_controller/nodes/sub_val_pub_angle_node.py
+++ auturbo_controller/nodes/sub_val_pub_angle_node.py
@@ -1,36 +1,34 @@
 import rospy
-from std_msgs.msg import String, Int32, Float32
+from std_msgs.msg import Int32
 
-class 패키지이름Node:
+class PurePursuitNode:
     def __init__(self):
-        print('Hello package')
-        rospy.Subscriber("distance_to_center", Float32, self.메세지콜백, queue_size=10)
-        self.pub_steering_angle = rospy.Publisher("steering_angle", Float32, queue_size=10)
-        rospy.Timer(rospy.Duration(0.1), self.timer_callback)
+        rospy.init_node('pure_pursuit_node', anonymous=True)
+        rospy.Subscriber("xycar_angle", Int32, self.angle_callback, queue_size=10)
+        self.pub_steering_angle = rospy.Publisher("steering_angle", Int32, queue_size=10)
         
-    def timer_callback(self, event):
-        # 여기서 pure pursuit 알고리즘을 구현합니다.
-        # distance_to_center를 이용하여 조향각을 계산합니다.
-        # 예를 들어, 간단히 현재 거리에 0.1을 더한 값을 조향각으로 사용하겠습니다.
-
-
-        #카메라 
+    def angle_callback(self, msg):
+        current_angle = msg.data
+        # 여기에 Pure Pursuit 알고리즘을 구현합니다.
+        # 현재 조향각인 current_angle과 목표값(0으로 가정)을 이용하여 알고리즘을 적용합니다.
+        # 다음 줄을 알고리즘에 맞게 수정해주세요.
         
-        steering_angle = self.current_distance + 0.1
+
+        target_angle = -5  # 여기에 Pure Pursuit 알고리즘을 적용하여 목표 조향각을 계산합니다.
+
+        
+        
+        # 현재 조향각과 목표 조향각과의 차이를 계산합니다.
+        diff_angle = target_angle - current_angle
 
         # 계산된 조향각을 메시지에 담아 발행합니다.
-        msg = Float32()
-        msg.data = steering_angle
-        self.pub_steering_angle.publish(msg)
+        steering_msg = Int32()
+        steering_msg.data = diff_angle
+        self.pub_steering_angle.publish(steering_msg)
     
-    def 메세지콜백(self, msg):
-        # 구독한 메시지인 distance_to_center를 클래스 변수로 저장합니다.
-        self.current_distance = msg.data
-    
-    def main(self):
+    def run(self):
         rospy.spin()
 
 if __name__ == '__main__':
-    rospy.init_node('패키지이름_node')
-    node = 패키지이름Node()
-    node.main()
+    node = PurePursuitNode()
+    node.run()
