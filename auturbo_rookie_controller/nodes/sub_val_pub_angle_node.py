cbcfdc0290ae975d143242663e66f8c03ab2ee06
ggh-png <0380089@gmail.com>
2023-07-25 17:46:51 +0900

    펄슛 update

--- auturbo_rookie_controller/nodes/sub_val_pub_angle_node.py
+++ auturbo_rookie_controller/nodes/sub_val_pub_angle_node.py
@@ -1,6 +1,7 @@
 import rospy
 from std_msgs.msg import Int32
 from xycar_msgs.msg import xycar_motor
+import math
 
 
 class PurePursuitNode:
@@ -10,6 +11,8 @@ class PurePursuitNode:
             "xycar_angle", Int32, self.angle_callback, queue_size=10
         )
         self.pub_steering_angle = rospy.Publisher("steering_angle", xycar_motor, queue_size=10)
+        self.WB = 15
+        self.Lf = 30
 
     def angle_callback(self, msg):
         current_angle = msg.data
@@ -21,11 +24,18 @@ class PurePursuitNode:
 
         # 현재 조향각과 목표 조향각과의 차이를 계산합니다.
         diff_angle = target_angle - current_angle
+        # 디그리 투 라디안
+        diff_angle = diff_angle * math.pi / 180
 
+        delta = math.atan2(2.0 * self.WB * math.sin(diff_angle), self.Lf)
         # 계산된 조향각을 메시지에 담아 발행합니다.
+        delta = delta * 180 / math.pi
         xycar_motor_msg = xycar_motor()
-        xycar_motor_msg.angle = int(diff_angle)
-        xycar_motor_msg.speed = 20
+        xycar_motor_msg.angle = int(delta)
+        if xycar_motor_msg.angle > 10:
+            xycar_motor_msg.speed = 25
+        else:
+            xycar_motor_msg.speed = 50
         self.pub_steering_angle.publish(xycar_motor_msg)
 
     def run(self):
