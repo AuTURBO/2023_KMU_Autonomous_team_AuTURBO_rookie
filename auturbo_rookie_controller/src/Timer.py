c4498b02962887cb20cc0acbd148e7f5c26d448a
ggh-png <0380089@gmail.com>
2023-08-08 09:49:18 +0900

    timer 주석 & 자이카 펄슛 모드 기능 추가

--- auturbo_rookie_controller/src/Timer.py
+++ auturbo_rookie_controller/src/Timer.py
@@ -8,16 +8,19 @@ class Timer(object):
     Timer Class for speed optimization
     '''
     def __init__(self):
+        # 초기시간 설정
         self.t0 = time.time()
 
     def update(self):
         '''
         update reference time
         '''
+        # 초기시간 시간 업데이트
         self.t0 = time.time()
 
     def __call__(self):
         '''
         return duration time since reference time
         '''
+        # 현재시간 - 초기시간
         return time.time() - self.t0
